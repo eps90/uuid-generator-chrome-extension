01d26bfdef1ccd03d0b405b7bfac301d4248b429
Kuba Turek <ja@kubaturek.pl>
2018-02-24 11:31:00 +0100

    Install and configure babel

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,12 @@ module.exports = {
         publicPath: "/",
         chunkFilename: "[name].[chunkhash].js"
     },
+    module: {
+        loaders: [
+            {
+                test: /\.js/, loader: "babel-loader", exclude: /node_modules/
+            }
+        ]
+    },
     devtool: "source-map"
 };
