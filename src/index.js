a3a59131669d730f5844a7bdae82e73cbf7b2181
Kuba Turek <ja@kubaturek.pl>
2018-02-24 20:23:34 +0100

    Add current stylesheets to the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+import "normalize.css/normalize.css";
+import "./styles/style.scss";
+
 ReactDOM.render(
     <App />,
     document.getElementById("root")
