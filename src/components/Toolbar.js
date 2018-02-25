a88b47b1420852872aa3ab876e70cd45c2bbf914
Kuba Turek <ja@kubaturek.pl>
2018-02-25 12:36:46 +0100

    Add refresh icon explicitly

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import {faSyncAlt} from "@fortawesome/fontawesome-free-solid";
 import PropTypes from "prop-types";
 
 const Toolbar = ({onRefresh}) => (
     <button className="toolbar-button toolbar-button--refresh" onClick={() => onRefresh()}>
-        <FontAwesomeIcon icon="sync-alt" />
+        <FontAwesomeIcon icon={faSyncAlt} />
     </button>
 );
 
