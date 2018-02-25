0c64f5bcecd0ac60261c7f8fd7c680357cc48b58
Kuba Turek <ja@kubaturek.pl>
2018-02-25 16:12:07 +0100

    Enter test mode with react-ga

--- config/setupEnzyme.js
+++ config/setupEnzyme.js
@@ -1,4 +1,6 @@
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import ReactGa from "react-ga";
 
 Enzyme.configure({adapter: new Adapter()});
+ReactGa.initialize("foo", {testMode: true});
