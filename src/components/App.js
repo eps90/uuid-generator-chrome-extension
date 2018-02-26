e163ada784c32f13e744255233c8c4da303ef966
Kuba Turek <ja@kubaturek.pl>
2018-02-26 21:40:25 +0100

    Insert footer

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,13 @@ import UuidComponent from "./UuidComponent";
 import generateUuid from "../uuid/generateUuid";
 
 import "./../styles/App.scss";
+import Footer from "./Footer";
 
 const App = () => {
     return (
         <div className="content">
             <UuidComponent generateUuid={generateUuid}/>
+            <Footer />
         </div>
     );
 };
