d7744b1527d71441e431b087f97b207d05fe4174
Kuba Turek <ja@kubaturek.pl>
2018-02-26 22:07:19 +0100

    Add FooterLink to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./../styles/Footer.scss";
+import FooterLink from "./FooterLink";
 
 const Footer = () => (
     <footer className="footer">
         <span className="footer__text">
-            Created by <a className="footer__link" href="https://github.com/eps90">Kuba Turek</a>
+            Created by <FooterLink className="footer__link" href="https://github.com/eps90" text="Kuba Turek"/>
         </span>
     </footer>
 );
