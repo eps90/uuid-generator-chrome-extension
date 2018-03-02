bed04a125403812de795982ed72712f38c989535
Kuba Turek <ja@kubaturek.pl>
2018-03-02 19:56:21 +0100

    Show react-clipboard component in toolbar

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import {faSyncAlt} from "@fortawesome/fontawesome-free-solid";
+import {faSyncAlt, faCopy} from "@fortawesome/fontawesome-free-solid";
 import PropTypes from "prop-types";
 import "./../styles/Toolbar.scss";
+import Clipboard from "react-clipboard.js";
 
-const Toolbar = ({onRefresh}) => (
-    <button className="toolbar-button toolbar-button--refresh" onClick={() => onRefresh()}>
-        <FontAwesomeIcon icon={faSyncAlt} fixedWidth/>
-    </button>
+const Toolbar = ({onRefresh, uuid}) => (
+    <div className="toolbar">
+        <button className="toolbar-button toolbar-button--refresh" onClick={() => onRefresh()}>
+            <FontAwesomeIcon icon={faSyncAlt} fixedWidth/>
+        </button>
+        <Clipboard data-clipboard-text={uuid} className="toolbar-button toolbar-button--copy">
+            <FontAwesomeIcon icon={faCopy} fixedWidth/>
+        </Clipboard>
+    </div>
 );
 
 Toolbar.propTypes = {
-    onRefresh: PropTypes.func.isRequired
+    onRefresh: PropTypes.func.isRequired,
+    uuid: PropTypes.string.isRequired
 };
 
 export default Toolbar;
--- src/components/Toolbar.test.js
+++ src/components/Toolbar.test.js
@@ -2,25 +2,38 @@ import React from "react";
 import Toolbar from "./Toolbar";
 import {shallow} from "enzyme";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import Clipboard from "react-clipboard.js";
 
 describe("Toolbar component", () => {
     it("should show refresh button", () => {
         const wrapper = shallow(getComponentUnderTest());
         expect(wrapper.find("button.toolbar-button--refresh")).toHaveLength(1);
-        expect(wrapper.find(FontAwesomeIcon)).toHaveLength(1);
+        expect(wrapper.findWhere(n => n.is(FontAwesomeIcon) && n.prop("icon").iconName === "sync-alt")).toHaveLength(1);
     });
 
     it("should call onRefresh function on click", () => {
-        const onRefreshMock = jest.fn();
-        const wrapper = shallow(getComponentUnderTest(onRefreshMock));
+        const onRefresh = jest.fn();
+        const wrapper = shallow(getComponentUnderTest({onRefresh}));
         wrapper.update();
 
         wrapper.find("button.toolbar-button--refresh").simulate("click");
 
-        expect(onRefreshMock).toHaveBeenCalled();
+        expect(onRefresh).toHaveBeenCalled();
     });
 
-    function getComponentUnderTest(onRefresh = () => {}) {
-        return <Toolbar onRefresh={onRefresh} />;
+    it("should show copy button", () => {
+        const uuid = "b813b672-f1d8-4b37-98db-9dd750393bbf";
+        const wrapper = shallow(getComponentUnderTest({uuid}));
+
+        expect(wrapper.find(Clipboard)).toHaveLength(1);
+        expect(wrapper.findWhere(n => n.is(FontAwesomeIcon) && n.prop("icon").iconName === "copy")).toHaveLength(1);
+    });
+
+    function getComponentUnderTest(properties = {}) {
+        const {
+            onRefresh = jest.fn(),
+            uuid = "b55fcf6c-21e7-45c7-bbe0-1261f0a30ee2"
+        } = properties;
+        return <Toolbar onRefresh={onRefresh} uuid={uuid} />;
     }
 });
