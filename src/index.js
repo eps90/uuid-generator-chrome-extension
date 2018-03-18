cda67be805593e22657f8cd193c242866327244e
Kuba Turek <ja@kubaturek.pl>
2018-03-18 10:23:17 +0100

    Initialize google analytics in a separate script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "./helper/ga";
+
 import "normalize.css/normalize.css";
 import "react-tippy/dist/tippy.css";
 import "./styles/index.scss";
@@ -5,11 +7,6 @@ import "./styles/index.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App/App";
-import ReactGa from "react-ga";
-
-const analyticsKey = process.env.GOOGLE_ANALYTICS_KEY || null;
-ReactGa.initialize(analyticsKey, {debug: process.env.NODE_ENV !== "production"});
-ReactGa.pageview("/");
 
 ReactDOM.render(
     <App />,
