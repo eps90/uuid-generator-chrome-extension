e83c17d36ad10d6108b66e53d7c390f7f6d2189b
Kuba Turek <ja@kubaturek.pl>
2018-03-27 21:37:47 +0200

    Introduce header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = () => (
+    <div className="header">
+        <span className="header__title">
+            UUID Generator
+        </span>
+    </div>
+);
+
+export default Header;
