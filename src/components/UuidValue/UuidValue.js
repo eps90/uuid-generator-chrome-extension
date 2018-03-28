4df32f7c1d738545d31ef66a3c3040eff43c8e2c
Kuba Turek <ja@kubaturek.pl>
2018-03-28 21:00:27 +0200

    Add copy button to UuidValue

--- src/components/UuidValue/UuidValue.js
+++ src/components/UuidValue/UuidValue.js
@@ -1,5 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import {faCopy} from "@fortawesome/fontawesome-free-regular";
+import Clipboard from "react-clipboard.js";
 
 import "./UuidValue.scss";
 
@@ -7,11 +10,17 @@ const handleFocus = (event) => {
     event.target.select();
 };
 
-const UuidValue = ({uuid}) => (
-    <input type="text" readOnly className="uuid-value" value={uuid} onFocus={handleFocus} />
+const UuidValue = ({uuid, onCopy}) => (
+    <div className="uuid">
+        <input type="text" size="36" readOnly className="uuid__value" value={uuid} onFocus={handleFocus} />
+        <Clipboard data-clipboard-text={uuid} className="uuid__toolbar-button" onClick={onCopy}>
+            <FontAwesomeIcon icon={faCopy} fixedWidth/>
+        </Clipboard>
+    </div>
 );
 UuidValue.propTypes = {
-    uuid: PropTypes.string.isRequired
+    uuid: PropTypes.string.isRequired,
+    onCopy: PropTypes.func.isRequired
 };
 
 export default UuidValue;
--- src/components/UuidValue/UuidValue.test.js
+++ src/components/UuidValue/UuidValue.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import UuidValue from "./UuidValue";
-import {shallow} from "enzyme";
+import {shallow, mount} from "enzyme";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome/index";
+import Clipboard from "react-clipboard.js";
 
 describe("UuidValue component", () => {
     it("should render an input", () => {
@@ -16,11 +18,32 @@ describe("UuidValue component", () => {
         expect(wrapper.find("input").first().prop("value")).toEqual(uuid);
     });
 
+    describe("Toolbar", () => {
+        it("should show copy button", () => {
+            const uuid = "b813b672-f1d8-4b37-98db-9dd750393bbf";
+            const wrapper = shallow(createComponentUnderTest({uuid}));
+
+            expect(wrapper.find(Clipboard)).toHaveLength(1);
+            expect(wrapper.findWhere(n => n.is(FontAwesomeIcon) && n.prop("icon").iconName === "copy")).toHaveLength(1);
+        });
+
+        it("should call onCopy function on successful copy", () => {
+            const onCopy = jest.fn();
+            const wrapper = mount(createComponentUnderTest({onCopy}));
+            wrapper.update();
+
+            wrapper.find("button.uuid__toolbar-button").simulate("click");
+
+            expect(onCopy).toHaveBeenCalled();
+        });
+    });
+
     function createComponentUnderTest(properties = {}) {
         const {
-            uuid = "82660ed3-d567-49b7-82fe-d40d08ae53f9"
+            uuid = "82660ed3-d567-49b7-82fe-d40d08ae53f9",
+            onCopy = jest.fn()
         } = properties;
 
-        return <UuidValue uuid={uuid}/>;
+        return <UuidValue uuid={uuid} onCopy={onCopy}/>;
     }
 });
