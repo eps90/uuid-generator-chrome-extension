8c90e75cf9744ae6b866f56d71b295f89ef62aac
Kuba Turek <ja@kubaturek.pl>
2018-03-28 20:25:54 +0200

    Select UUID value on click

--- src/components/UuidValue/UuidValue.js
+++ src/components/UuidValue/UuidValue.js
@@ -3,8 +3,12 @@ import PropTypes from "prop-types";
 
 import "./UuidValue.scss";
 
+const handleFocus = (event) => {
+    event.target.select();
+};
+
 const UuidValue = ({uuid}) => (
-    <input type="text" readOnly className="uuid-value" value={uuid} />
+    <input type="text" readOnly className="uuid-value" value={uuid} onFocus={handleFocus} />
 );
 UuidValue.propTypes = {
     uuid: PropTypes.string.isRequired
